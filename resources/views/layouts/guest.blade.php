288307930906dccef89aaa2bf91cf0b17a21c66e
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-07-23 03:44:52 +0200

    fix assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,7 @@
         <!-- Scripts -->
         <link rel="stylesheet" href="https://nasssdev.github.io/laravel-app-assets/css/app.css">
         <script src="https://nasssdev.github.io/laravel-app-assets/js/app.js" defer></script>
+        <script src="https://cdn.tailwindcss.com" defer></script>
 {{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     </head>
     <body class="font-sans text-gray-900 antialiased">
